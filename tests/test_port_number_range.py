d8d6f5b63755911ab3b3c4149eea422880d6750a
s204166 <s204166@student.dtu.dk>
2023-02-28 09:18:40 +0100

    added skelton error case test file

--- tests/test_port_number_range.py
+++ tests/test_port_number_range.py
@@ -97,23 +97,7 @@ class TestPortNumberRange(unittest.TestCase):
         
         self.assertEqual(self.policyFactory.getRuleTuple(),expectedOutput.read())
 
-    def test_wildcard_and_ranges(self):
-        self.init3Trees()
-        rule0 = ["*","*","3","alpha"]    
-        rule1 = ["0","2","*","beta"]    
-        rule2 = ["*","*","*","gamma"]    
-        self.policyFactory.insertRange(rule0)
-        self.policyFactory.insertRange(rule1)
-        self.policyFactory.insertRange(rule2)
 
-        self.policyFactory.writeCodewords()
-        self.hashTable = cuckooHashTable.CuckooHashTable()
-        for rule in self.policyFactory.previousRuleTuple:
-            self.hashTable.insert(rule[1],rule[0])
-
-        logging.debug("hashtable lookup"+str(self.hashTable.lookup(["8","2","3"])))
-        logging.debug(self.policyFactory.getRuleTuple())
-        
         
     def init3Trees(self):
        self.tree0 = portnumbertrie.PortNumberTrieTree()

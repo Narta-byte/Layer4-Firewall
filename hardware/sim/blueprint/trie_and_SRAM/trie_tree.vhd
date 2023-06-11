f5c99608e87420f5991e6b32b1c258f6ea03c10a
Narta_byte <s204166@student.dtu.dk>
2023-06-11 21:54:45 +0200

    cleanup

--- hardware/sim/blueprint/trie_and_SRAM/trie_tree.vhd
+++ hardware/sim/blueprint/trie_and_SRAM/trie_tree.vhd
@@ -59,7 +59,6 @@ architecture rtl of trie_tree_logic is
     alias codeword_from_memory : std_logic_vector is data_from_memory(codeword_length + address_width*2 - 1 downto address_width*2);
     alias zeroPointer : std_logic_vector is data_from_memory(address_width*2 - 1 downto address_width);
     alias onePointer : std_logic_vector is data_from_memory(address_width - 1 downto 0);
-    -- alias DEBUG_seq0 : std_logic_vector is data_from_memory(1 downto 0);
     
     signal codeword_zeros : std_logic_vector(codeword_length -1 downto 0) := (others => '0') ;
     
@@ -73,7 +72,6 @@ architecture rtl of trie_tree_logic is
     signal key_reg : std_logic_vector(key_length - 1 downto 0);
     signal address_reg : std_logic_vector(address_width - 1 downto 0);
     signal reg, reg_next : std_logic := '0';
-    -- signal iterations : integer range 0 to max_iterations := 0;
 begin
 
 
@@ -86,7 +84,6 @@ begin
         current_state <= idle_state;
 
       elsif rising_edge(clk) then
-        -- eof_key_flag <= eof_key_flag_next; --FIX ME
         current_state <= next_state;
       end if;
     end process;
@@ -101,37 +98,24 @@ begin
         if (vld_north = '1') and lock_next = '0' then
           next_state <= buffer_read_state;
           lock<= '0';
-        -- elsif (vld_collect_header = '1') and lock_next = '1' then
-        --   next_state <= idle_state;
-
-        --   lock<= '1';
         end if;
       when buffer_read_state =>next_state <= fetch_state;
       when output_state => 
       if rdy_south = '1'  then
         next_state <= idle_state;
-      -- elsif rdy_codeword_concatinator = '1'  then
-      --   next_state <= output_state;
       else
         next_state <= output_state;
       end if;
 
 
       when read_state =>
-      if (eof_key_flag_next = '1' ) then--and rdy_codeword_concatinator = '1' then
-        -- next_state <= idle_state;
-        -- rdy_collect_header <= '1';
-      -- elsif (eof_key_flag_next = '1' ) and rdy_codeword_concatinator = '0' then
+      if (eof_key_flag_next = '1' ) then
         next_state <= output_state;
       else
         next_state <= fetch_state;
         end if;
       when fetch_state =>
-        -- if eof_key_flag_next = '1' then
-        --   next_state <= idle_state;
-        -- else
           next_state <= read_state;
-        -- end if;
         
       when others => next_state <= idle_state;
   
@@ -144,19 +128,10 @@ begin
     
       case(current_state) is
         when idle_state => 
-          -- if rdy_codeword_concatinator = '1' then
-          --   rdy_collect_header <= '1';
-          -- else
-          --   rdy_collect_header <= '0';
-          -- end if;
           rdy_north <= '1';
           reg <= '0';
-          -- codeword <= (others => '0');
           eof_key_flag <= '0';
-          -- address_reg <= input_address;
-          address <= input_address; -- address get transfered incorrectly 
-          -- address <= (others => '0');
-          -- key_cnt <= 0;
+          address <= input_address; 
           
           vld_south <= '0';
           
@@ -180,21 +155,16 @@ begin
           
 
 
-          -- output_codeword <= codeword_from_memory;
-          -- iterations <= 0;
         when buffer_read_state => 
           rdy_north <= '0';
-          -- address <= address_reg;
           key_reg <= key_in;
           
         when output_state => 
         vld_south <= '1';
         codeword <= best_codeword;
-        -- key_cnt <= key_cnt;
         output_codeword <= best_codeword;
         key_out <= key_reg;
         output_address <= address_reg;
-        -- rdy_collect_header <= '1'; -- maybe this should be here
         reg <= reg_next;
         if islast then
           reg_next <= '0';
@@ -221,38 +191,24 @@ begin
         end if; 
         
         if  eof_key_flag_next = '1' then
-          -- vld_codeword_concatinator <= '1';
-          -- codeword <= best_codeword;
-          -- -- key_cnt <= key_cnt;
-          -- output_codeword <= best_codeword;
-          -- key_out <= key_reg;
-          -- rdy_collect_header <= '1';
-          -- address_reg <= address;
         else
           rdy_north <= '0';
-          -- elsif (iterations = max_iterations - 1) then
-          --   codeword <= best_codeword;
-          --   key_out <= key_in;
             
 
           end if;   
 
 
         when fetch_state =>
-          -- iterations <= iterations + 1; -- why not just reuse keycount?
 
           rdy_north <= '0';
           codeword <= best_codeword;
-          -- output_codeword <= best_codeword; -- jeg tror at hvis man tilføjer et delay så virker det
 
           DEBUG_bool_max_iter <= key_cnt = key_length - 1 or key_cnt = max_iterations -1;
           if key_cnt = key_length - 1 or key_cnt = max_iterations -1 then
-            -- report "max iterations reached";
             eof_key_flag_next <= '1';
-            -- vld_codeword_concatinator <= '1';
 
             -- output_address
-            key_cnt_out <= std_logic_vector(to_unsigned(key_cnt, 5)); -- this could be improved
+            key_cnt_out <= std_logic_vector(to_unsigned(key_cnt, 5)); 
             report "key_cnt_out = " & integer'image(key_cnt);
 
           else
@@ -262,20 +218,15 @@ begin
           DEBUG_bool_00 <= '0';
           DEBUG_bool_01 <= '0';
           DEBUG_bool_02 <= '0';
-          -- report "key_in(key_cnt) = " & std_logic'image(key_in(key_cnt));
           if zeroPointer = zeros and onePointer = zeros then
             address_reg <= zeroPointer;
             DEBUG_bool_00 <= '1';
-            -- codeword <= best_codeword;
-            -- vld_codeword_concatinator <= '1';
             eof_key_flag_next <= '1';
           elsif key_in(key_cnt) = '0'  then
             address <= zeroPointer;
             address_reg <= zeroPointer;
             if zeroPointer = zeros then
               DEBUG_bool_01 <= '1';
-              -- codeword <= best_codeword;
-              -- vld_codeword_concatinator <= '1';
               eof_key_flag_next <= '1';
             end if;
           elsif key_in(key_cnt) = '1' then
@@ -283,12 +234,9 @@ begin
             address <= onePointer;
             address_reg <= onePointer;
             if onePointer = zeros then
-              -- codeword <= best_codeword;
-              -- vld_codeword_concatinator <= '1';
               eof_key_flag_next <= '1';
             end if;
           end if;
-          -- codeword <= (others => '0');
 
         when others => report "ERROR IN OUTPUT LOGIC" severity failure;
     
